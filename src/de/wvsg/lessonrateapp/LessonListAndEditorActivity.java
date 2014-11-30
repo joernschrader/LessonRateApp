7b73c1bbc32f02e7e5f72baa9035ac53664a17bc
schrader <joern.schrader@bildungsmagnet.de>
2014-11-30 22:48:58 +0100

    New classed added

---
+++ src/de/wvsg/lessonrateapp/LessonListAndEditorActivity.java
@@ -0,0 +1,14 @@
+package de.wvsg.lessonrateapp;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class LessonListAndEditorActivity extends FragmentActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lesson_list_and_editor);
+	}
+
+}
